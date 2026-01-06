104d8b68f5bbcaef2a5b39b3d6f461865907cace
saliyasuresh99-a11y <saliyasuresh99@gmail.com>
2026-01-06 19:34:14 +0200

    Update Interop.Libraries.cs

--- src/Interop/Interop.Libraries.cs
+++ src/Interop/Interop.Libraries.cs
@@ -1,11 +1,1 @@
-internal static partial class Interop
-{
-    private static class Libraries
-    {
-#if IOS || TVOS || MACCATALYST
-        internal const string Libsodium = "__Internal";
-#else
-        internal const string Libsodium = "libsodium";
-#endif
-    }
-}
+Issue Tracker Content Removal
